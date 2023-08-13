176f5a77aecfc6a0d4983b19facd590fe6ef7077
Ananya P <72087709+ananyaprao@users.noreply.github.com>
2023-08-13 23:32:32 +0530

    Add files via upload

---
+++ m.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//this brings in the iostream library
+int main()
+{
+    std::cout<<"hello world"<<std::endl;
+    //prints hello world to console
+   /*
+this is a block comment 
+   */
+
+    std::cout<<"number 1";
+    return 0;
+}
